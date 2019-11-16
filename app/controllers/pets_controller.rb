e1ee8262100d8e2bc14dada9d8ada8241886585d
Thiago <thiago.feijo13@gmail.com>
2019-11-16 01:15:33 -0300

    chore: green step pet index

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,15 @@
 class PetsController < ApplicationController
+  before_action :pets, only: :index
+  
+  # GET /
+  def index; end
+
+  private
+
+  def pets
+    @pets ||= Pet
+      .paginate(page: params[:page])
+      .order(params[:sort])
+  end
+
 end
